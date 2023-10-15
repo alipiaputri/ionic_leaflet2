15b8fde691c4f7229c30d2c2f12b1a38cb4e5d1a
Alipia Putri <alipia.putri.riyanti@mail.ugm.ac.id>
2023-10-15 09:06:34 +0700

    ionic leaflet
    ionic leaflet perbaikan marker

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  map!: L.Map;
+  baseMap!: any;
+
+  constructor() {}
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([51.5, -0.09], 10);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
+
+    this.baseMap = {
+      OpenStreetMap: L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }
+      ),
+      Topo: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.opentopomap.org/copyright">OpenTopoMap</a> contributors',
+      }),
+      Sattelite: L.tileLayer(
+        'https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+        {
+          maxZoom: 20,
+          subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+          attribution:
+            '&copy; <a href="https://maps.google.com/">Google Maps</a>',
+        }
+      ),
+    };
+
+    this.baseMap['OpenStreetMap'].addTo(this.map);
+
+    L.control.layers(this.baseMap).addTo(this.map);
+
+    const marker = L.circle([51.5, -0.09], 500, {
+      color: 'red',
+      fillColor: 'red',
+    });
+
+    const popup = L.popup().setContent('popup Alipia');
+
+    marker.bindPopup(popup);
+
+    this.map.addLayer(marker);
+  }
+}
